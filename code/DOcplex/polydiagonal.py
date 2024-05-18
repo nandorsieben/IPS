43632dec2f6fba69c5b92a2e58870a90e83c15a4
nandorsieben <nandor.sieben@gmail.com>
2024-05-18 12:03:25 -0700

    Add files via upload

---
+++ code/DOcplex/polydiagonal.py
@@ -0,0 +1,36 @@
+# Finds coloring vectors of polydiagonal subspaces invariant under the matrix Mat
+from docplex.cp.model import CpoModel
+from read_matrix import read_matrix
+
+Mat=read_matrix('M.txt')
+n=len(Mat)
+mdl=CpoModel(name='polydiagonal')
+
+# coloring vector decision variables
+c=[mdl.integer_var(min=-i,max=i+1,name=f"c_{i}") for i in range(n)]
+# max values
+m=[mdl.max([c[j] for j in range(i)]) for i in range(n)]
+# bases
+b=[[mdl.conditional(c[i]==k,1,mdl.conditional(c[i]==-k,-1,0))
+    for i in range(n)] for k in range(n+1)]
+# Mat*bases
+w=[[mdl.sum(Mat[i][j]*b[k][j] for j in range(n)) for i in range(n)] for k in range(n+1)]
+
+# enforce coloring vector
+mdl.add(0<=c[0],c[0]<=1)
+for i in range(1,n):
+    mdl.add(-m[i]<=c[i],c[i]<=1+m[i])
+
+# enforce invariance
+for k in range(1,n+1):
+    for i in range(n):
+        mdl.add(mdl.if_then(c[i]==0,w[k][i]==0))
+        for j in range(i):
+            mdl.add(mdl.if_then(c[i]==c[j],w[k][i]==w[k][j]))
+            mdl.add(mdl.if_then(c[i]==-c[j],w[k][i]==-w[k][j]))
+
+# mdl.print_information()
+
+siter=mdl.start_search(SearchType='DepthFirst',Workers=1,LogVerbosity='Quiet')
+for sol in siter:
+    print(*[sol[c[i]] for i in range(n)])
---
+++ code/DOcplex/readme.txt
@@ -0,0 +1,14 @@
+Requirements:
+
+1. DOcplex (https://en.wikipedia.org/wiki/CPLEX)
+It can be isntalled with:
+pip3 install docplex
+
+2. cpoptimizer
+It is part of the IBM ILOG Cplex optimization software. It has a free academic licence.
+
+
+Usage:
+python3 polydiagonal.py
+python3 synchrony.py
+The code reads a matrix from the file M.txt and prints the coloring vectors to standard output.
