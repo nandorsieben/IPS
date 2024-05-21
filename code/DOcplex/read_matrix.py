687afe17f73e194e1c604e54554828dce5ac9b53
nandorsieben <nandor.sieben@gmail.com>
2024-05-21 09:29:42 -0700

    Delete code/DOcplex/read_matrix.py

--- code/DOcplex/read_matrix.py
+++
@@ -1,7 +0,0 @@
-def read_matrix(filename):
-    Mat=[]
-    with open(filename, "r") as f:
-        for line in f:
-            integer_tuple = tuple(int(x) for x in line.strip().split())
-            Mat.append(integer_tuple)
-    return Mat
